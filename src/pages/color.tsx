e293667f922dc4a0883a0602949bbb3c09a656b0
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-06-22 21:17:34 +0100

    updated the color section
    In this change, I updated the section page

--- src/pages/color.tsx
+++ src/pages/color.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import { roboto_mono } from "@/styles/fonts";
+import { GetColorName } from "hex-color-to-color-name";
 
 interface ColorInfo {
   hex: string;
   rgb: string;
+  colorName: string;
 }
 
 const generateRandomColor = () => {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  const hex = `#${r.toString(16).padStart(2, "0")}${g.toString(16).padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
+  const hex = `#${r.toString(16).padStart(2, "0").toUpperCase()}${g.toString(16).padStart(2, "0").toUpperCase()}${b.toString(16).padStart(2, "0").toUpperCase()}`;
   const rgb = `rgb(${r},${g},${b})`;
-  return { hex, rgb };
+  const colorName = GetColorName(hex); // returns 'Black'
+
+  return { hex, rgb, colorName };
 };
 
 const ColorList = () => {
   const [colors, setColors] = useState<ColorInfo[]>([]);
-  const [baseColor, setBaseColor] = useState<ColorInfo>(generateRandomColor());
   const [secondaryColors, setSecondaryColors] = useState<ColorInfo[]>([]);
+  const [tertiaryColors, setTertiaryColors] = useState<ColorInfo[]>([]);
 
   const generateColors = () => {
     const newColors = [];
@@ -31,19 +35,27 @@ const ColorList = () => {
       newSecondaryColors.push(generateRandomColor());
     }
     setSecondaryColors(newSecondaryColors);
+    const newTertiaryColors = [];
+    for (let i = 0; i < 5; i++) {
+      newTertiaryColors.push(generateRandomColor());
+    }
+    setTertiaryColors(newTertiaryColors);
   };
 
   return (
     <div className={`${roboto_mono.className}`}>
-      <h1 className="text-center text-2xl">Random colors</h1>
+      <h1 className="text-center text-2xl">A World of Vibrant Hues</h1>
       <ul className="flex justify-center items-center pt-4">
         {colors.map((color: ColorInfo, index) => (
           <li key={index} className="flex justify-center items-center flex-col">
             <div
               style={{ backgroundColor: color.hex }}
-              className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.25rem]"
+              className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.4rem]"
             />
-            <p className="text-[.9rem]"> {color.hex}</p>
+            <p className="text-[.9rem] max-sm:text-[.65rem] max-sm:text-center 	">
+              {color.colorName}
+            </p>
+            <p className="text-[.9rem] text"> {color.hex}</p>
             <p className="text-[.9rem] max-sm:hidden">{color.rgb}</p>
           </li>
         ))}
@@ -55,6 +67,27 @@ const ColorList = () => {
               style={{ backgroundColor: color.hex }}
               className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.25rem]"
             />
+            <p className="text-[.9rem] max-sm:text-[.65rem] max-sm:text-center 	">
+              {color.colorName}
+            </p>
+            <p className="text-[.9rem]"> {color.hex}</p>
+            <p className="text-[.9rem] max-sm:hidden">{color.rgb}</p>
+          </li>
+        ))}
+      </ul>
+      <ul className="flex justify-center items-center pt-4">
+        {tertiaryColors.map((color: ColorInfo, index) => (
+          <li
+            key={index}
+            className="flex justify-center items-center flex-col "
+          >
+            <div
+              style={{ backgroundColor: color.hex }}
+              className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.25rem]"
+            />
+            <p className="text-[.9rem] max-sm:text-[.65rem] max-sm:text-center">
+              {color.colorName}
+            </p>
             <p className="text-[.9rem]"> {color.hex}</p>
             <p className="text-[.9rem] max-sm:hidden">{color.rgb}</p>
           </li>
