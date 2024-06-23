083f781299446d41920107cf0fb2c9bb8112e4cc
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-06-23 11:17:22 +0100

    made the colors section page responsive
    In this change, I made the colors section responsive

--- src/pages/color.tsx
+++ src/pages/color.tsx
@@ -23,85 +23,95 @@ const ColorList = () => {
   const [colors, setColors] = useState<ColorInfo[]>([]);
   const [secondaryColors, setSecondaryColors] = useState<ColorInfo[]>([]);
   const [tertiaryColors, setTertiaryColors] = useState<ColorInfo[]>([]);
+  const [generated, setGenerated] = useState(false);
 
   const generateColors = () => {
     const newColors = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 6; i++) {
       newColors.push(generateRandomColor());
     }
     setColors(newColors);
     const newSecondaryColors = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 6; i++) {
       newSecondaryColors.push(generateRandomColor());
     }
     setSecondaryColors(newSecondaryColors);
     const newTertiaryColors = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 6; i++) {
       newTertiaryColors.push(generateRandomColor());
     }
     setTertiaryColors(newTertiaryColors);
+    setGenerated(true);
   };
 
   return (
     <div className={`${roboto_mono.className}`}>
-      <h1 className="text-center text-2xl">A World of Vibrant Hues</h1>
-      <ul className="flex justify-center items-center pt-4">
-        {colors.map((color: ColorInfo, index) => (
-          <li key={index} className="flex justify-center items-center flex-col">
-            <div
-              style={{ backgroundColor: color.hex }}
-              className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.4rem]"
-            />
-            <p className="text-[.9rem] max-sm:text-[.65rem] max-sm:text-center 	">
-              {color.colorName}
-            </p>
-            <p className="text-[.9rem] text"> {color.hex}</p>
-            <p className="text-[.9rem] max-sm:hidden">{color.rgb}</p>
-          </li>
-        ))}
-      </ul>
-      <ul className="flex justify-center items-center pt-4">
-        {secondaryColors.map((color: ColorInfo, index) => (
-          <li key={index} className="flex justify-center items-center flex-col">
-            <div
-              style={{ backgroundColor: color.hex }}
-              className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.25rem]"
-            />
-            <p className="text-[.9rem] max-sm:text-[.65rem] max-sm:text-center 	">
-              {color.colorName}
-            </p>
-            <p className="text-[.9rem]"> {color.hex}</p>
-            <p className="text-[.9rem] max-sm:hidden">{color.rgb}</p>
-          </li>
-        ))}
-      </ul>
-      <ul className="flex justify-center items-center pt-4">
-        {tertiaryColors.map((color: ColorInfo, index) => (
-          <li
-            key={index}
-            className="flex justify-center items-center flex-col "
-          >
-            <div
-              style={{ backgroundColor: color.hex }}
-              className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.25rem]"
-            />
-            <p className="text-[.9rem] max-sm:text-[.65rem] max-sm:text-center">
-              {color.colorName}
-            </p>
-            <p className="text-[.9rem]"> {color.hex}</p>
-            <p className="text-[.9rem] max-sm:hidden">{color.rgb}</p>
-          </li>
-        ))}
-      </ul>
-      <div className="flex justify-center items-center my-4">
-        <button
-          type="submit"
-          onClick={generateColors}
-          className="bg-[var(--primary-color)] hover:text-[var(--secondary-color)]
+      <h2 className="text-center text-2xl">A World of Vibrant Hues</h2>
+      <div className="max-w-[100%] w-[calc(100% - 2rem)] m-auto">
+        <ul className="grid grid-cols-6 gap-4 pt-4 max-sm:grid-cols-3 max-sm:gap-2 mx-4 max-md:grid-cols-3 max-lg:grid-cols-3 max-xl:grid-cols-4">
+          {colors.map((color: ColorInfo, index) => (
+            <li
+              key={index}
+              className="flex justify-center items-center flex-col mx-4"
+            >
+              <div
+                style={{ backgroundColor: color.hex }}
+                className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.4rem]"
+              />
+              <p className="text-[.9rem] max-sm:text-[.65rem] max-sm:text-center 	">
+                {color.colorName}
+              </p>
+              <p className="text-[.9rem] max-sm:text-[.65rem]  ">
+                {" "}
+                {color.hex}
+              </p>
+              <p className="text-[.9rem] max-sm:hidden">{color.rgb}</p>
+            </li>
+          ))}
+          {secondaryColors.map((color: ColorInfo, index) => (
+            <li
+              key={index}
+              className="flex justify-center items-center flex-col"
+            >
+              <div
+                style={{ backgroundColor: color.hex }}
+                className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.25rem]"
+              />
+              <p className="text-[.9rem] max-sm:text-[.65rem] max-sm:text-center 	">
+                {color.colorName}
+              </p>
+              <p className="text-[.9rem] max-sm:text-[.65rem] "> {color.hex}</p>
+              <p className="text-[.9rem] max-sm:hidden">{color.rgb}</p>
+            </li>
+          ))}
+          {tertiaryColors.map((color: ColorInfo, index) => (
+            <li
+              key={index}
+              className="flex justify-center items-center flex-col "
+            >
+              <div
+                style={{ backgroundColor: color.hex }}
+                className="w-[200px] h-[200px] mx-[.75rem] max-sm:w-[85px] max-sm:h-[85px] max-sm:mx-[.25rem]"
+              />
+              <p className="text-[.9rem] max-sm:text-[.65rem] max-sm:text-center">
+                {color.colorName}
+              </p>
+              <p className="text-[.9rem] max-sm:text-[.65rem] "> {color.hex}</p>
+              <p className="text-[.9rem] max-sm:hidden">{color.rgb}</p>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex justify-center items-center my-4">
+          <button
+            type="submit"
+            onClick={generateColors}
+            className="bg-[var(--primary-color)] hover:text-[var(--secondary-color)]
          text-[var(--light)] px-4 py-2 rounded-xl"
-        >
-          Generate
-        </button>
+          >
+            {generated ? "Regenerate" : "Generate"}
+          </button>
+        </div>
       </div>
     </div>
   );
