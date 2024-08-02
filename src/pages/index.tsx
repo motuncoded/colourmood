11f5fb6a12951d6fcee26f02f475f72df8a402e4
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-08-02 08:54:42 +0100

    feat: set up the color mood section
    In this change, I set up the color mood to change the mood along withe the colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
+import {
+  useState 
+} from "react";
+
+
 import Hero from "../components/Hero";
 import Mood from "../components/Mood";
 
 export default function Home() {
+
+  const [selectedMood, setSelectedMood] = useState<string>("");
+
+  const handleMoodChange = (newMood: string) => {
+    setSelectedMood(newMood);
+  };
+
   return (
     <div>
       <Hero />
-      <Mood />
+      <Mood selectedMood={selectedMood} />
     </div>
   );
 }
